53962bc76eeeefd37af36c71091e7995a3f93789
nkovacic <niko.kovacic2@gmail.com>
2019-11-14 21:32:12 +0100

    Properly linked Spotify sdk.

--- android/app/src/main/java/com/spotify/challenge/app/MainApplication.java
+++ android/app/src/main/java/com/spotify/challenge/app/MainApplication.java
@@ -3,6 +3,8 @@ package com.spotify.challenge.app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lufinkey.react.spotify.RNSpotifyPackage;;
+import com.lufinkey.react.eventemitter.RNEventEmitterPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
@@ -29,6 +31,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSpotifyPackage(),
+            new RNEventEmitterPackage(),
             new RNGestureHandlerPackage(),
             new SplashScreenReactPackage(),
             new VectorIconsPackage(),
